abddec132a9fc9ba87765f5eee3e4e9b26526381
efe19 <efe19@DESKTOP-D3JTPLB>
2022-11-22 12:36:51 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+
+	//Generic constraint (generic kısıt)
+	//class = referans tip olmak zorunda (int vs. yazamaz)
+	//IEntity = T ya IEntity olabilir yada ondan impelemente olan bir şey olabilir.
+	//new = new'lenebilir olmalı
+	public interface IEntityRepository<T> where T : class,IEntity,new()
+	{
+		List<T> GetAll(Expression<Func<T,bool>> filter = null);
+		T Get(Expression<Func<T,bool>> filter);
+		void Add(T Entity);
+		void Delete(T Entity);
+		void Update(T Entity);
+
+	}
+}
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class EfProductDal : IProductDal
+	{
+		public void Add(Product Entity)
+		{
+			using (NorthwindContext context = new NorthwindContext())
+			{
+				var addedEntity = context.Entry(Entity);
+				addedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Added;
+				context.SaveChanges();	
+			}
+		}
+
+		public void Delete(Product Entity)
+		{
+			using (NorthwindContext context = new NorthwindContext())
+			{
+				var deletedEntity = context.Entry(Entity);
+				deletedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+				context.SaveChanges();
+			}
+		}
+
+		public Product Get(Expression<Func<Product, bool>> filter)
+		{
+			using (NorthwindContext context = new NorthwindContext())
+			{
+				return context.Set<Product>().SingleOrDefault(filter);
+			}
+		}
+
+		public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+		{
+			using (NorthwindContext context = new NorthwindContext())
+			{
+				return filter == null 
+					? context.Set<Product>().ToList() 
+					: context.Set<Product>().Where(filter).ToList();
+			}
+		}
+
+		public void Update(Product Entity)
+		{
+			using (NorthwindContext context = new NorthwindContext())
+			{
+				var updatedEntity = context.Entry(Entity);
+				updatedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+				context.SaveChanges();
+			}
+		}
+	}
+}
